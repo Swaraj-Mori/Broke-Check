b60259bfb01f9beba08f3b38945496364742a9d9
Swarajsinh <160823446+Swaraj-Mori@users.noreply.github.com>
2026-08-11 18:27:33 +0530

    Update views.py

--- website/views.py
+++ website/views.py
@@ -185,7 +185,7 @@ def edit_category():
         
     if category_name in categories:
         flash('Category already exists', category='error')
-    elif len(category_name) < 1 old_category_name != "General":
+    elif len(category_name) < 1 and old_category_name != "General":
         flash('Category name too short', category='error')
     elif int(category_budget) < 1:
         flash('Budget must be greater than 0', category='error')
