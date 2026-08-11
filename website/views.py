fc7d44f90b2ff91ea8ab48e3dbd04d62446423ae
Swarajsinh <160823446+Swaraj-Mori@users.noreply.github.com>
2026-08-11 17:57:25 +0530

    Update views.py

--- website/views.py
+++ website/views.py
@@ -181,26 +181,26 @@ def edit_category():
     categories = user.categories.split(',')
     budgets = user.budgets.split(',')
 
-    if category_name in categories:
-        flash('Category already exists', category='error')
-    elif len(category_name) < 1:
-        flash('Category name too short', category='error')
-    elif int(category_budget) < 1:
-        flash('Budget must be greater than 0', category='error')
-    elif old_category_name in categories:
+    budgets[categories.index(old_category_name)] = category_budget
+    new_budgets = ""
 
-        budgets[categories.index(old_category_name)] = category_budget
-        new_budgets = ""
+    for i, budget in enumerate(budgets):
+        new_budgets += f',{budget}' if i > 0 else budget
+        
+    user.budgets = new_budgets    
 
-        for i, budget in enumerate(budgets):
-            new_budgets += f',{budget}' if i > 0 else budget
-            
-        user.budgets = new_budgets
+    if old_category_name != "General":        
+        
+        category_name = category_name.capitalize()
+        
+        if category_name in categories:
+            flash('Category already exists', category='error')
+        elif len(category_name) < 1:
+            flash('Category name too short', category='error')
+        elif int(category_budget) < 1:
+            flash('Budget must be greater than 0', category='error')
+        elif old_category_name in categories:
 
-        if old_category_name != "General":
-
-            category_name = category_name.capitalize()
-            
             Expense.query.filter_by(category=old_category_name, user_id=current_user.id).update({"category": category_name})
 
             categories[categories.index(old_category_name)] = category_name
@@ -211,7 +211,7 @@ def edit_category():
             
             user.categories = new_categories
 
-        flash('Category edited', category='success')
+    flash('Category edited', category='success')
     db.session.commit()
 
     return redirect(url_for('views.budget'))
